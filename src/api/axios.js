e6b10531195651cc58f9a41f9b31e7552289515a
Dev Amal <57208761+AMALKSANTHOSH@users.noreply.github.com>
2026-04-04 23:19:44 +0530

    updated backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Change this one line when deploying to production
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://ocym-backend.onrender.com';
 
 const api = axios.create({ baseURL: BASE_URL });
 
